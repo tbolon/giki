e7bdf64f6dcbcef9d70ee466706a4ad9aad48092
Thomas Bolon <thomas.bolon@gmail.com>
2015-05-21 22:45:35 +0200

    gestion tables + layout

--- src/Giki.Console.Legacy/GikiParser.cs
+++ src/Giki.Console.Legacy/GikiParser.cs
@@ -1,25 +1,136 @@
-﻿using System.Text.RegularExpressions;
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Giki
 {
-    internal static class GikiParser
-    {
-        public static string Parse(string content)
-        {
-            content = Regex.Replace(content, @"\[\[(?<Link>[^|\]]+)(\|(?<Text>\|[^\]]+))?\]\]", InnerLinkReplace);
+	internal static class GikiParser
+	{
+		public static string Parse(string content)
+		{
+			// wikilinks
+			content = Regex.Replace(content, @"\[\[(?<Link>[^|\]]+)(\|(?<Text>[^\]]+))?\]\]", InnerLinkReplace);
 
-            return content;
-        }
+			// tables
+			content = Regex.Replace(content, @"^{\|(.*?)\n\|}\r?$", TableReplace, RegexOptions.Multiline | RegexOptions.Singleline | RegexOptions.CultureInvariant);
 
-        private static string InnerLinkReplace(Match match)
-        {
-            var link = match.Groups["Link"].Value;
-            var text = match.Groups["Text"].Success ? match.Groups["Text"].Value : link;
+			return content;
+		}
 
-            link = Regex.Replace(link, @"['-]", string.Empty, RegexOptions.CultureInvariant);
-            link = Regex.Replace(link, @"[\s]", "-", RegexOptions.CultureInvariant);
+		private static string InnerLinkReplace(Match match)
+		{
+			var link = match.Groups["Link"].Value;
+			var text = match.Groups["Text"].Success ? match.Groups["Text"].Value : link;
 
-            return string.Format("[{0}]({1}.html)", text, link);
-        }
-    }
+			link = Regex.Replace(link, @"['-]", string.Empty, RegexOptions.CultureInvariant);
+			link = Regex.Replace(link, @"[\s]", "-", RegexOptions.CultureInvariant);
+
+			return string.Format("[{0}]({1}.html)", text, link);
+		}
+
+		private static string TableReplace(Match match)
+		{
+			var builder = new StringBuilder(match.Length);
+			var hasTr = false;
+			var inTd = false;
+
+			foreach (var line in match.Groups[0].Value.Split('\n'))
+			{
+				if (line.StartsWith("{|"))
+				{
+					var attr = line.Substring(2).Trim();
+					if (!string.IsNullOrEmpty(attr))
+					{
+						builder.AppendFormat("<table {0}>", attr);
+					}
+					else
+					{
+						builder.AppendFormat("<table>");
+					}
+				}
+				else if (line.StartsWith("|}"))
+				{
+					builder.AppendLine("</table>");
+				}
+				else if (line.StartsWith("|+"))
+				{
+					builder.Append("<caption>")
+						.Append(line.Substring(2).Trim().ToLowerInvariant())
+						.AppendLine("</caption>");
+				}
+				else if (line.StartsWith("|-"))
+				{
+					if (inTd)
+						builder.AppendLine().AppendLine("</td>");
+
+					if (hasTr)
+						builder.AppendLine("</tr>");
+
+					var attr = line.Substring(2).Trim();
+					if (!string.IsNullOrEmpty(attr))
+					{
+						builder.AppendFormat("<tr {0}>", attr.ToLowerInvariant()).AppendLine();
+					}
+					else
+					{
+						builder.AppendLine("<tr>");
+					}
+
+					hasTr = true;
+				}
+				else if (line.StartsWith("|"))
+				{
+					if (inTd)
+						builder.AppendLine().AppendLine("</td>");
+
+					if (!hasTr)
+						builder.AppendLine("<tr>");
+
+					
+					var lineContent = line.Substring(2).Trim();
+
+					var cells = lineContent.Split(new[] { "||" } ,  StringSplitOptions.None );
+					if (cells.Length > 1)
+					{
+						foreach (var cell in cells)
+						{
+							builder.AppendLine("<td>").AppendLine();
+							builder.Append(cell.Trim());
+							builder.AppendLine().AppendLine("</td>");
+						}
+					}
+					else
+
+					{
+						var pipeIndex = lineContent.IndexOf('|');
+						if (pipeIndex > 0)
+						{
+							builder.Append("<td ")
+								.Append(lineContent.Substring(0, pipeIndex-1).ToLowerInvariant())
+								.AppendLine(">").AppendLine();
+
+							lineContent = lineContent.Substring(pipeIndex + 1);
+						}
+						else
+						{
+
+							builder.AppendLine("<td>").AppendLine();
+						}
+
+						builder.Append(lineContent);
+
+
+
+						inTd = true;
+					}
+				}
+				else
+				{
+					builder.Append(line).Append('\n');
+				}
+			}
+
+			return builder.ToString();
+		}
+	}
 }
--- src/Giki.Console.Legacy/Program.cs
+++ src/Giki.Console.Legacy/Program.cs
@@ -1,64 +1,81 @@
 ﻿using System;
 using System.IO;
 using DotLiquid;
+using System.Text;
+using System.Linq;
 
 namespace Giki
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var root = args[0];
-            var outDir = Path.Combine(root, "_out");
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var root = args[0];
+			var outDir = Path.Combine(root, "_out");
 
-            Directory.CreateDirectory(outDir);
+			Directory.CreateDirectory(outDir);
 
-            Template.FileSystem = new SnippetsFileSystem(Path.Combine(root, "_snippets"));
-            foreach (var file in Directory.GetFiles(root, "*.xmd"))
-            {
-                // 0) content
-                var content = File.ReadAllText(file);
+			var layout = Template.Parse(File.ReadAllText(Path.Combine(root, "_layouts", "default.html")));
 
-                // 1) snippable parsing
-                var parts = Snippable.Parse(content, new[] { "yaml", "md" });
-                content = parts[1].Content;
+			Template.FileSystem = new SnippetsFileSystem(Path.Combine(root, "_snippets"));
+			foreach (var file in Directory.GetFiles(root, "*.xmd"))
+			{
+				Console.WriteLine(Path.GetFileNameWithoutExtension(file));
 
-                // 2) liquid parsing
-                var template = Template.Parse(content);
-                content = template.Render();
+				// 0) content
+				var content = File.ReadAllText(file, Encoding.UTF8);
 
-                // 3) custom markup parsing
-                content = GikiParser.Parse(content);
+				var hash = new Hash();
 
-                // 4) commonmark parsing
-                content = CommonMark.CommonMarkConverter.Convert(content);
+				// 1) snippable parsing
+				var parts = Snippable.Parse(content, new[] { "yaml", "md" });
+				content = parts[1].Content;
 
-                // 5) write result
-                File.WriteAllText(Path.Combine(outDir, Path.GetFileNameWithoutExtension(file) + ".html"), content);
-            }
-        }
-    }
+				foreach (var link in parts[0].Content.Split('\n').Select(l => l.Trim()))
+				{
+					var keys = link.Split(':');
+					hash[keys[0].Trim()] = string.Join(":", keys.Skip(1)).Trim();
+				}
 
-    class SnippetsFileSystem : DotLiquid.FileSystems.IFileSystem
-    {
-        private readonly string dir;
+				// 2) liquid parsing
+				var template = Template.Parse(content);
+				content = template.Render();
 
-        public SnippetsFileSystem(string dir)
-        {
-            this.dir = dir;
-        }
-        public string ReadTemplateFile(Context context, string templateName)
-        {
-            var path = Path.Combine(dir, templateName + ".xmd");
-            if (!File.Exists(path))
-            {
-                return string.Empty;
-            }
+				// 3) custom markup parsing
+				content = GikiParser.Parse(content);
 
-            var content = File.ReadAllText(path);
-            var snips = Snippable.Parse(content, new[] { "yaml", "md" });
+				// 4) commonmark parsing
+				content = CommonMark.CommonMarkConverter.Convert(content);
 
-            return snips[1].Content;
-        }
-    }
+				hash["content"] = content;
+				content = layout.Render(hash);
+
+				// 5) write result
+				File.WriteAllText(Path.Combine(outDir, Path.GetFileNameWithoutExtension(file) + ".html"), content);
+			}
+		}
+	}
+
+	class SnippetsFileSystem : DotLiquid.FileSystems.IFileSystem
+	{
+		private readonly string dir;
+
+		public SnippetsFileSystem(string dir)
+		{
+			this.dir = dir;
+		}
+		public string ReadTemplateFile(Context context, string templateName)
+		{
+			var path = Path.Combine(dir, templateName + ".xmd");
+			if (!File.Exists(path))
+			{
+				return string.Empty;
+			}
+
+			var content = File.ReadAllText(path);
+			var snips = Snippable.Parse(content, new[] { "yaml", "md" });
+
+			return snips[1].Content;
+		}
+	}
 }
